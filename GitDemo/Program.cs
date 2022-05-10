f76bdef807216b0ec7e7afe239e2011a41a19a35
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-10 14:20:56 +0200

    Add project files.

---
+++ GitDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace GitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int res = Sum(4, 5);
+        }
+
+        private static int Sum(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
