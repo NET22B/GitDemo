aa04b5ef8d54de4560fc1520638d9688848d76f0
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-10 14:25:22 +0200

    Remove Sum()

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -7,12 +7,8 @@ namespace GitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int res = Sum(4, 5);
         }
 
-        private static int Sum(int v1, int v2)
-        {
-            return v1 + v2;
-        }
+      
     }
 }
